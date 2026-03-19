f1813336ee2e5bd54bfed3084511324bacc63ae9
Annuum <keoknowsis@gmail.com>
2026-03-20 01:07:45 +0800

    Basic Gameobject layout added idk

---
+++ gameobject.py
@@ -0,0 +1,21 @@
+
+# Libraries
+import pygame as _pygame
+from abc import ABC, abstractmethod
+
+# Code
+class GameObject:
+    pass
+
+class GOCollection:
+    pass
+
+class GORenderer:
+    pass
+
+class GOUpdater:
+    pass
+
+# Testing
+if __name__ == "__main__":
+    pass
